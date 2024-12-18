d225be4cdfb7608a1ece546a55a65984077835a6
fandi a <fandiiardy@gmail.com>
2024-12-18 12:13:52 +0700

    finish the blog

--- Config/config.php
+++ Config/config.php
@@ -1,19 +1,31 @@
 <?php
 
 try {
+    //host
     $host = "localhost";
-    $dbname = "MyBlog";
+
+    //dbname
+    $dbname = "myblog";
+
+    //user
     $user = "root";
+
+    //pass
     $pass = "";
+
 
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // if ($conn == true) {
-    //     echo "Connected to the database";
-    // } else {
-    //     echo "Failed to connect to the database";
-    // }
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+
+
+    
+
+
+    // if($conn == true) {
+    //     echo "conn works fine";
+    // } else {
+    //     echo "conn err";
+    // }
